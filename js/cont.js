bff02374d9bd3ccb8c730aaeed18cf0a30b77295
PnzJust <panzaru.iustin@yahoo.com>
2020-01-02 17:21:55 +0200

    .

--- js/cont.js
+++ js/cont.js
@@ -3,3 +3,16 @@ function afiseaza_data() {
 	let new_data = new Date;
 	x.innerHTML = new_data.toDateString();
 }
+
+function start(){
+	// classa Math
+	let x = document.getElementById("sold");
+	x.innerHTML = "SOLD: " + Math.round(Math.random() * 1000) + "$";
+	
+	// clasa String
+	x = document.getElementById("nume");
+	var nume = "nume: bbbbb";
+	nume = nume.replace("bbbbb", "aaaa");
+	nume = nume.toUpperCase();
+	x.innerHTML = nume;
+}
