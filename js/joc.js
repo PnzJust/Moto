f536fa8a5fafbec03139edf572bc9c699fb9f16a
PnzJust <panzaru.iustin@yahoo.com>
2020-01-04 20:50:18 +0200

    /

--- js/joc.js
+++ js/joc.js
@@ -19,6 +19,15 @@ function start(){
 		var dimensiuni = window.getComputedStyle(this, null);
 		alert("Dimensiunile parintelui sunt " + dimensiuni["height"] +" x " + dimensiuni["width"]);
 	},true);
+
+
+	var anii = [2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011];
+	for (x of anii)
+	{
+		text = document.getElementById("ani").innerHTML;
+		text = text + x + " ";
+		document.getElementById("ani").innerHTML = text;
+	}
 }
 
 var ABC = setInterval(SchimbaCuloarea, 300, Math.floor(Math.random()*1000%256));
@@ -30,4 +39,22 @@ function SchimbaCuloarea(parametru){
 
 function STOP(){
 	clearInterval(ABC);
+}
+
+
+function change_to_classA(){
+	var v = document.getElementById("ani");
+	if (v.classList.contains("classB")){
+		v.classList.remove("classB");
+		v.className = "classA";
+	}
+}
+
+function change_to_classB(){
+	var v = document.getElementById("ani");
+	if (v.classList.contains("classA")){
+		v.classList.remove("classA");
+		v.className = "classB";
+	}
+
 }
